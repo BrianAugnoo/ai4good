22625480c68a30cc27e65ba4cdf5762f920d7d07
Brian Augnoo <augnoobrian@gmail.com>
2025-11-08 02:26:09 +0400

    Fix Render build pipeline

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
-
-# Exit on error
+# exit on error
 set -o errexit
 
+# Installe les dépendances Ruby et Node
 bundle install
-bin/rails assets:precompile
-bin/rails assets:clean
+yarn install --check-files
+
+# Compile les fichiers CSS et JS
+yarn build:css
+yarn build
+
+# Précompile les assets Rails
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+bundle exec rails db:prepare
 
 # If you have a paid instance type, we recommend moving
 # database migrations like this one from the build command
