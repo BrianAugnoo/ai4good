2f0803e338a1508536103671f13935b23f13ad7b
Brian Augnoo <augnoobrian@gmail.com>
2025-11-08 16:06:33 +0400

    Revert "Fix Render build pipeline"
    This reverts commit 22625480c68a30cc27e65ba4cdf5762f920d7d07.

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/env bash
-# exit on error
+
+# Exit on error
 set -o errexit
 
-# Installe les dépendances Ruby et Node
 bundle install
-yarn install --check-files
-
-# Compile les fichiers CSS et JS
-yarn build:css
-yarn build
-
-# Précompile les assets Rails
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-bundle exec rails db:prepare
+bin/rails assets:precompile
+bin/rails assets:clean
 
 # If you have a paid instance type, we recommend moving
 # database migrations like this one from the build command
