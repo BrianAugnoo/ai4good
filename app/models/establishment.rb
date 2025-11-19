70669e088c0c64a3c2918c1a559f2287f1961025
Brian Augnoo <augnoobrian@gmail.com>
2025-11-19 05:27:08 +0400

    massive bugs

--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -4,6 +4,7 @@ class Establishment < ApplicationRecord
   has_many :examiners, through: :establishment_criteria
 
   has_one_attached :logo
+  has_many_attached :videos
 
   def valid_eval?
     self.examiners.uniq.count == Examiner.where(permited: true).count
