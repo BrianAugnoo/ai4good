6d7c74b7f31b5dd22eaa335e16c68b162c9a5575
Brian Augnoo <augnoobrian@gmail.com>
2025-11-21 19:47:35 +0400

    mailer prod test

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "contact@ai4good.app"
   layout "mailer"
 end
--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -1,5 +1,4 @@
 class TestMailer < ApplicationMailer
-  default from: "contact@ai4good.app"
   def test
     mail(to: "brianchan337@gmail.com", subject: "test")
   end
